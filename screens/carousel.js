9cc2620d6645a1f978c5f3a010efbcd5722a92f4
Het Gajera <het36100@gmail.com>
2023-09-12 22:23:39 +0530

    Update carousel.js

--- screens/carousel.js
+++ screens/carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { StyleSheet, Dimensions, View, Text, Image, TouchableOpacity } from "react-native";
 import Carousel, { Pagination } from "react-native-snap-carousel";
 
@@ -9,29 +9,30 @@ import splash3 from "../assets/images/splash3.png";
 const DATA = [
   {
     image: splash1,
-    title: "Lorem Ipsum",
-    content: "lorem le lorem le ipsum la ipsum la ipsum la",
-    width: 280, 
-    height: 280, 
+    title: "Plant Care",
+    content: "Simplify disease diagnosis and crop protection with easy access to plant health records.",
+    width: 280,
+    height: 280,
   },
   {
     image: splash2,
-    title: "Lorem Ipsum",
-    content: "lorem le lorem le ipsum la ipsum la ipsum la",
-    width: 280, 
+    title: "Scan & Detect",
+    content: "Use AI for quick plant disease detection, ensuring healthier and more abundant harvests.",
+    width: 280,
     height: 280,
   },
   {
     image: splash3,
-    title: "Lorem Ipsum",
-    content: "lorem le lorem le ipsum la ipsum la ipsum la",
-    width: 350, 
+    title: "Crop Precision",
+    content: "Customized solutions for plant diseases, optimizing crop yields with precision and personalized care.",
+    width: 350,
     height: 280,
   },
 ];
 
 export default function SplashCarousel({ navigation }) {
   const [activeSlide, setActiveSlide] = useState(0);
+  const carouselRef = useRef(null);
 
   const renderItem = ({ item }) => (
     <View style={styles.card}>
@@ -43,12 +44,22 @@ export default function SplashCarousel({ navigation }) {
     </View>
   );
 
+  const handleArrowPress = () => {
+    if (activeSlide < DATA.length - 1) {
+      carouselRef.current.snapToNext();
+    } else {
+      navigation.navigate('Login');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Image 
-      style={styles.logo}
-      source={require("../assets/images/logo.png")} />
+      <Image
+        style={styles.logo}
+        source={require("../assets/images/logo.png")}
+      />
       <Carousel
+        ref={carouselRef}
         data={DATA}
         renderItem={renderItem}
         sliderWidth={Dimensions.get("window").width}
@@ -65,10 +76,11 @@ export default function SplashCarousel({ navigation }) {
           inactiveDotScale={1}
           dotStyle={styles.dot}
         />
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <TouchableOpacity onPress={handleArrowPress}>
           <Image 
-             style={{ height: 50, width: 50 }}
-            source={require("../assets/images/arrow.png")} />
+            style={styles.arrow}
+            source={require("../assets/images/arrow.png")}
+          />
         </TouchableOpacity>
       </View>
     </View>
@@ -80,11 +92,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    paddingTop: 80,
+    paddingTop: "15%",
     paddingBottom: 50,
   },
   card: {
     alignItems: "center",
+  },
+  textContainer: {
+    textAlign: "left",
+    paddingHorizontal: 10,
   },
   title: {
     marginTop: "10%",
@@ -95,7 +111,7 @@ const styles = StyleSheet.create({
   content: {
     textAlign: "left",
     paddingTop: 10,
-    fontSize: 24,
+    fontSize: 18,
     fontFamily: "Poppins_500Medium",
   },
   image: {
@@ -105,14 +121,15 @@ const styles = StyleSheet.create({
     height: 85,
     width: 135,
   },
-  textContainer: {
-    alignItems: "center",
-  },
   dot: {
     backgroundColor: "#049A10",
     width: 10,
     height: 10,
     borderRadius: 10,
+  },
+  arrow: {
+    width: 50,
+    height: 50,
   },
   bottom: {
     flexDirection: "row",
