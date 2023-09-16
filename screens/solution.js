2cc2d7b4795d97fe477a99f62a0193c8f07fc89a
ironnicko <nickonicko779@gmail.com>
2023-09-16 23:15:06 +0530

    Solution update

--- screens/solution.js
+++ screens/solution.js
@@ -4,24 +4,18 @@ import names from "../assets/data/model_classes.json";
 
 export default function Solution({route, navigation}) {
   const data = route.params.obj;
-  var diseaseName = "";
-  for(let c in names[data.name][data.class_id]){
-    if (names[data.name][data.class_id][c] != '_')
-      diseaseName += names[data.name][data.class_id][c];
-    else
-      diseaseName += ' ';
-  }
+  var diseaseName = names[data.name][data.class_id];
+
   console.log(route);
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.card1}>
         <Text style={styles.title}>DISEASE</Text>
-        <Text style={styles.name}>NAME</Text>
-        <Text style={styles.info}>{diseaseName}</Text>
+        <Text style={styles.name}>{diseaseName}</Text>
       </View>
         <View style={styles.card2}>
           <Text style={styles.title}>SOLUTION</Text>
-          <Text style={styles.name}>NAME</Text>
+          <Text style={styles.name}>METHODS</Text>
           <ScrollView>
             <Text style={styles.info}>{data.solution}</Text>
           </ScrollView>
@@ -33,19 +27,19 @@ export default function Solution({route, navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
+    justifyContent: "flex",
     alignItems: "stretch",
-    paddingTop: "4%",
   },
   card1: {
     backgroundColor: "#F2F2F2",
-    marginBottom: "40%",
+    marginBottom: "5%",
     padding: "4%",
-    marginTop: "12%",
+    marginTop: "5%",
   },
   card2: {
     backgroundColor: "#F2F2F2",
     padding: "4%",
+    marginBottom : "90%"
   },
   title: {
     fontSize: 64,
@@ -53,12 +47,12 @@ const styles = StyleSheet.create({
     color: "#049A10",
   },
   name: {
-    marginTop: "-8%",
+    marginTop: "-10%",
     fontSize: 45,
     fontFamily: "Poppins_600SemiBold",
   },
   info: {
-    marginTop: "-2%",
+    marginTop: "2%",
     fontFamily: "Poppins_500Medium",
     fontSize: 20,
   },
