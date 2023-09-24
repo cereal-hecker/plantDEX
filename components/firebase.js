10fc5275126f2b3ee3aab7d6762c6e3b9d83d74e
ironnicko <nickonicko779@gmail.com>
2023-09-24 16:54:15 +0530

    backend integration for profile[display name]

---
+++ components/firebase.js
@@ -0,0 +1,25 @@
+import {initializeApp} from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7ofjR3HSEBRwwqDJgGHemFD_JePF0qYU",
+  authDomain: "plant-dex-9e9e8.firebaseapp.com",
+  projectId: "plant-dex-9e9e8",
+  storageBucket: "plant-dex-9e9e8.appspot.com",
+  messagingSenderId: "295245831860",
+  appId: "1:295245831860:web:3a842285210569a53941e1",
+  measurementId: "G-J81VGJ6KCM"
+};
+
+// Initialize Firebase
+
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider(app);
+export const db = getFirestore(app);
--- components/profileOverlay.js
+++ components/profileOverlay.js
@@ -1,167 +1,192 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-Modal,
-View,
-Text,
-TouchableOpacity,
-StyleSheet,
-TextInput,
-Image,
-} from 'react-native';
-import * as ImagePicker from 'expo-image-picker'; // Assuming you are using Expo
-import { Ionicons } from '@expo/vector-icons'; // Assuming you are using Expo
+  Modal,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Image,
+} from "react-native";
+import { updateProfile } from "firebase/auth";
+import { auth, firebaseConfig, db } from "./firebase";
+import {
+  collection,
+  setDoc,
+  doc,
+  updateDoc,
+  query,
+  orderBy,
+  startAfter,
+  limit,
+  getDocs,
+  getDoc,
+} from "firebase/firestore";
+import * as ImagePicker from "expo-image-picker"; // Assuming you are using Expo
+import { Ionicons } from "@expo/vector-icons"; // Assuming you are using Expo
 
 const ProfileOverlay = () => {
-const [modalVisible, setModalVisible] = useState(false);
-const [userName, setUserName] = useState('John Doe');
-const [phoneNumber, setPhoneNumber] = useState('123-456-7890');
-const [email, setEmail] = useState('john.doe@example.com');
-const [image, setImage] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [userName, setUserName] = useState(auth.currentUser.displayName);
+  const [phoneNumber, setPhoneNumber] = useState("123-456-7890");
+  const [email, setEmail] = useState("john.doe@example.com");
+  const [image, setImage] = useState(null);
 
-const pickImage = async () => {
+  const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ImagePicker.MediaTypeOptions.All,
-    allowsEditing: true,
-    aspect: [4, 3],
-    quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
 
     if (!result.cancelled) {
-    setImage(result.uri);
+      setImage(result.uri);
     }
-};
+  };
 
-const handleUpdate = () => {
-    console.log('called')
-};
+  const handleUpdate = async () => {
+    // If the username is actually being changed
+    try {
+      if (userName != auth.currentUser.displayName) {
+        updateProfile(auth.currentUser, { displayName: userName });
+      }
 
-return (
+      alert("Successfully Updated");
+    } catch (e) {
+      alert("Error Occured!");
+    }
+  };
+
+  return (
     <View style={styles.centeredView}>
-    <TouchableOpacity onPress={() => setModalVisible(true)}>
-        <Image
-            source={require("../assets/images/account.png")}
-            />
-    </TouchableOpacity>
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <Image source={require("../assets/images/account.png")} />
+      </TouchableOpacity>
 
-    <Modal
+      <Modal
         animationType="slide"
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
-        setModalVisible(!modalVisible);
+          setModalVisible(!modalVisible);
         }}
-    >
+      >
         <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-            <TouchableOpacity onPress={pickImage} style={styles.profileImageContainer}>
-            {image ? (
+          <View style={styles.modalView}>
+            <TouchableOpacity
+              onPress={pickImage}
+              style={styles.profileImageContainer}
+            >
+              {image ? (
                 <Image source={{ uri: image }} style={styles.profileImage} />
-            ) : (
-                <Image
-                    source={require("../assets/images/account.png")}
-                    />  
-            )}
+              ) : (
+                <Image source={require("../assets/images/account.png")} />
+              )}
             </TouchableOpacity>
 
             <TextInput
-            style={styles.input}
-            value={userName}
-            onChangeText={setUserName}
-            placeholder="User Name"
+              style={styles.input}
+              value={userName}
+              onChangeText={setUserName}
+              placeholder="User Name"
             />
             <TextInput
-            style={styles.input}
-            value={phoneNumber}
-            onChangeText={setPhoneNumber}
-            placeholder="Phone Number"
-            keyboardType="phone-pad"
+              style={styles.input}
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+              placeholder="Phone Number"
+              keyboardType="phone-pad"
             />
             <TextInput
-            style={styles.input}
-            value={email}
-            onChangeText={setEmail}
-            placeholder="Email"
-            keyboardType="email-address"
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Email"
+              keyboardType="email-address"
             />
 
-            <TouchableOpacity style={styles.updateButton} onPress={handleUpdate}>
-            <Text style={styles.updateButtonText}>Update</Text>
+            <TouchableOpacity
+              style={styles.updateButton}
+              onPress={handleUpdate}
+            >
+              <Text style={styles.updateButtonText}>Update</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
-            style={styles.closeButton}
-            onPress={() => setModalVisible(!modalVisible)}
+              style={styles.closeButton}
+              onPress={() => setModalVisible(!modalVisible)}
             >
-            <Text style={styles.closeButtonText}>Close</Text>
+              <Text style={styles.closeButtonText}>Close</Text>
             </TouchableOpacity>
+          </View>
         </View>
-        </View>
-    </Modal>
+      </Modal>
     </View>
-);
+  );
 };
 
 const styles = StyleSheet.create({
-centeredView: {
+  centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 22,
-},
-modalView: {
+  },
+  modalView: {
     margin: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 20,
     padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
-    width: 0,
-    height: 2,
+      width: 0,
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-},
-input: {
+  },
+  input: {
     height: 40,
     width: 200,
     margin: 12,
     borderWidth: 1,
     padding: 10,
-},
-profileImageContainer: {
+  },
+  profileImageContainer: {
     width: 80,
     height: 80,
     borderRadius: 40,
     marginBottom: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#e0e0e0',
-},
-profileImage: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#e0e0e0",
+  },
+  profileImage: {
     width: 80,
     height: 80,
     borderRadius: 40,
-},
-updateButton: {
-    backgroundColor: '#2196F3',
+  },
+  updateButton: {
+    backgroundColor: "#2196F3",
     padding: 10,
     borderRadius: 20,
     marginTop: 10,
-},
-updateButtonText: {
-    color: 'white',
-},
-closeButton: {
-    backgroundColor: 'red',
+  },
+  updateButtonText: {
+    color: "white",
+  },
+  closeButton: {
+    backgroundColor: "red",
     padding: 10,
     borderRadius: 20,
     marginTop: 10,
-},
-closeButtonText: {
-    color: 'white',
-},
+  },
+  closeButtonText: {
+    color: "white",
+  },
 });
 
 export default ProfileOverlay;
