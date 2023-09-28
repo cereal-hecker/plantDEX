e1591babe69dd02eb1a0fa8ae0f721a5b08da29f
Riya Batla <riyabatla72@gmail.com>
2023-09-28 21:27:05 +0530

    add history detail

--- components/historyCard.js
+++ components/historyCard.js
@@ -1,11 +1,24 @@
-import React from "react";
-import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
 
 const HistoryCard = ({ item }) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   return (
     <ScrollView>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={toggleModal}>
         <View style={styles.card}>
           <Image
             source={require("../assets/images/logo.png")}
@@ -18,6 +31,28 @@ const HistoryCard = ({ item }) => {
           </View>
         </View>
       </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={toggleModal}
+      >
+        <ScrollView>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalname}>
+              {item.cropName}
+            </Text>
+            <Text style={styles.modaldisease}>
+            {item.diseaseName}
+            </Text>
+            <Text style={styles.modalsolution}>{item.solution}</Text>
+            <TouchableOpacity onPress={toggleModal} style={styles.closeButton}>
+              <Text style={styles.closetext}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </Modal>
     </ScrollView>
   );
 };
@@ -43,6 +78,19 @@ const styles = StyleSheet.create({
     height: 81,
     margin: 5,
   },
+  modalname: {
+    fontFamily: "Poppins_900Black",
+    fontSize: 40,
+  },
+  modaldisease: {
+    marginTop: -10,
+    fontFamily: "Poppins_700Bold",
+    fontSize: 32,
+    color: "#049A10"
+  },
+  modalsolution: {
+    fontFamily: "Poppins_500Medium",
+  },
   textContainer: {
     flex: 2,
     padding: 10,
@@ -52,9 +100,28 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 5,
   },
-  description: {
-    fontSize: 14,
+  modalContainer: {
+    padding: 30,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
+  modalText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "black",
+  },
+  closeButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#049A10",
+    borderRadius: 8,
+  },
+  closetext: {
+    color: "white"
+  }
 });
 
 export default HistoryCard;
